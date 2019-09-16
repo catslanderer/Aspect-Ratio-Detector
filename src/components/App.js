0c7e998e9c7676d8a8f569205ab5ed4e34092b99
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-09-15 23:38:09 -0400

    Added Title.js component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import Title from './Title';
 import Board from './Board';
 import Controls from './Controls';
 
 function App() {
   return (
     <div className='App'>
+      <Title />
       <Board />
       <Controls />
     </div>
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Title() {
+  return (
+    <div className={'title'}>
+      <p className={'text'}>Title</p>
+    </div>
+  );
+}
