f5f06bf0ffa88d1d781e5fbc544c65005c7c9653
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-09-25 00:14:53 -0400

    Board component now display the current aspect ratio range. Styled text to best fit a variety of screen sizes.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export default function Board() {
   return (
     <div className={'board'}>
-      <p className={'text'}>Board</p>
+      <p className={'medium-text'}>Viewport Aspect Ratio:</p>
+      {useMediaQuery('(max-aspect-ratio:3/4)') && (
+        <p className={'large-text'}>&lt; 3/4</p>
+      )}
+      {useMediaQuery('(min-aspect-ratio:3/4) and (max-aspect-ratio:4/3)') && (
+        <p className={'large-text'}>&gt;= 3/4 and &lt; 4/3</p>
+      )}
+      {useMediaQuery('(min-aspect-ratio:4/3)') && (
+        <p className={'large-text'}>&gt;= 4/3</p>
+      )}
+      <p className={'small-text'}>Board</p>
     </div>
   );
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title() {
   return (
     <div className={'title'}>
-      <p className={'text'}>Aspect Ratio Detector</p>
+      <p className={'small-text'}>Aspect Ratio Detector</p>
     </div>
   );
 }
