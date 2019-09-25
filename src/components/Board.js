f1fd547cf4a62edb8ce9e95897c9f376964c3e20
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-09-25 01:26:09 -0400

    I realized while writing the README.md that there is a better way to divide the ranges of aspect ratios than what I'd been doing. Specifically, there are 4 ranges now: <3/4, 3/4 to 1/1, 1/1 to 4/3 and >4/3. Splitting the original middle range into two ranges allows the app to use more of the screen between 1/1 and 4/3 aspect ratios while not adding a new layout.

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,12 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 export default function Board() {
   const lessThan34 = useMediaQuery('(max-aspect-ratio:3/4)');
   const exactly34 = useMediaQuery('(aspect-ratio:3/4)');
-  const between34and43 = useMediaQuery(
-    '(min-aspect-ratio:3/4) and (max-aspect-ratio:4/3)'
+  const between34and11 = useMediaQuery(
+    '(min-aspect-ratio:3/4) and (max-aspect-ratio:1/1)'
+  );
+  const exactly11 = useMediaQuery('(aspect-ratio:1/1)');
+  const between11and43 = useMediaQuery(
+    '(min-aspect-ratio:1/1) and (max-aspect-ratio:4/3)'
   );
   const exactly43 = useMediaQuery('(aspect-ratio:4/3)');
   const greaterThan43 = useMediaQuery('(min-aspect-ratio:4/3)');
@@ -13,8 +17,11 @@ export default function Board() {
     <div className={'board'}>
       <p className={'medium-text'}>Viewport Aspect Ratio:</p>
       {lessThan34 && !exactly34 && <p className={'large-text'}>&lt; 3/4</p>}
-      {between34and43 && !exactly43 && (
-        <p className={'large-text'}>&gt;= 3/4 and &lt; 4/3</p>
+      {between34and11 && !exactly11 && (
+        <p className={'large-text'}>&gt;= 3/4 and &lt; 1/1</p>
+      )}
+      {between11and43 && !exactly43 && (
+        <p className={'large-text'}>&gt;= 1/1 and &lt; 4/3</p>
       )}
       {greaterThan43 && <p className={'large-text'}>&gt;= 4/3</p>}
       <p className={'small-text'}>Board</p>
